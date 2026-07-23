64f67cbfd095b8deb285d75e3c9b3336979c197e
hamdyano993 <hamdyano993@gmail.com>
2026-07-24 02:54:01 +0300

    adding  work

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config();
 
 
 const app = express();
-app.use(cors());
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Frontend origin
+      credentials: true, // Allow credentials (cookies) to be sent
+    })
+  );
+
+  
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 
 app.get("/api/test", async (req, res) => {
   res.json({ message: "Test successful!" });
